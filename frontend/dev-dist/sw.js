114c24353be1aca6abfcd07bae3f4286c6f57f63
Ravi Shankar <rs1806105@gmail.com>
2025-11-22 13:03:55 +0530

    feat: Add PWA service worker and comprehensive application routing with Redux, Auth, and Material-UI theming.

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-9dc17825'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ukdcekk3m6g"
+    "revision": "0.3p221trba48"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
